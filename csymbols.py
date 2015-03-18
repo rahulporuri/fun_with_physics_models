773c09a5240af749803344026616f3671a9e65a0
rahul.poruri <rahul.poruri@gmail.com>
2015-03-18 16:47:26 +0530

    working christffel symbols computation for plane polar metric

--- csymbols.py
+++ csymbols.py
@@ -4,30 +4,19 @@ r, phi = sympy.symbols('r phi')
 
 gmn= sympy.Matrix([[1,0],[0,r**2]])
 gMN= gmn**(-1)
+indices = {0:'r', 1:'phi'}
 
-sum = 0
-for o in [r, phi]:
-	if o == r:
-		j = 0
-		print (1./2)*gMN[2*0+j]*(sympy.diff(gmn[2*1+j],r) +sympy.diff(gmn[2*1+j],r) -sympy.diff(gmn[2*1+1],r))
-		sum += (1./2)*gMN[2*0+j]*(sympy.diff(gmn[2*1+j],r) +sympy.diff(gmn[2*1+j],r) -sympy.diff(gmn[2*1+1],r))
-	if o == phi:
-		j = 1
-		print (1./2)*gMN[2*0+j]*(sympy.diff(gmn[2*1+j],phi) +sympy.diff(gmn[2*1+j],phi) -sympy.diff(gmn[2*1+1],phi))
-		sum += (1./2)*gMN[2*0+j]*(sympy.diff(gmn[2*1+j],phi) +sympy.diff(gmn[2*1+j],phi) -sympy.diff(gmn[2*1+1],phi))
-
-print sum
-
-sum = 0
-for o in [r, phi]:
-        if o == r:
-                j = 0
-                print (1./2)*gMN[2*1+j]*(sympy.diff(gmn[2*0+j],phi) +sympy.diff(gmn[2*1+j],r) -sympy.diff(gmn[2*0+1],r))
-		sum += (1./2)*gMN[2*1+j]*(sympy.diff(gmn[2*0+j],phi) +sympy.diff(gmn[2*1+j],r) -sympy.diff(gmn[2*0+1],r))
-        if o == phi:
-                j = 1
-		sum += (1./2)*gMN[2*1+j]*(sympy.diff(gmn[2*0+j],phi) +sympy.diff(gmn[2*1+j],r) -sympy.diff(gmn[2*0+1],phi)) 
-                print (1./2)*gMN[2*1+j]*(sympy.diff(gmn[2*0+j],phi) +sympy.diff(gmn[2*1+j],r) -sympy.diff(gmn[2*0+1],phi))
-print sum
-
-
+for i in [0,1]:
+	for j in [0,1]:
+		for k in [0,1]:
+			sum = 0
+			for o in [r, phi]:
+				if o == r:
+					l = 0
+			#                print (1./2)*gMN[2*1+j]*(sympy.diff(gmn[2*0+j],phi) +sympy.diff(gmn[2*1+j],r) -sympy.diff(gmn[2*0+1],r))
+					sum += (1./2)*gMN[2*k+l]*(sympy.diff(gmn[2*j+l],indices[i]) +sympy.diff(gmn[2*i+l],indices[j]) -sympy.diff(gmn[2*i+j],indices[l]))
+				if o == phi:
+					l = 1
+					sum += (1./2)*gMN[2*k+l]*(sympy.diff(gmn[2*j+l],indices[i]) +sympy.diff(gmn[2*i+l],indices[j]) -sympy.diff(gmn[2*i+j],indices[l]))
+			#                print (1./2)*gMN[2*1+j]*(sympy.diff(gmn[2*0+j],phi) +sympy.diff(gmn[2*1+j],r) -sympy.diff(gmn[2*0+1],phi))
+			print 'gamma',i,j,k,sum
